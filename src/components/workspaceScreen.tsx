7be055ce645040d856d083db22edbdf6a4e0b0c8
Michael LeMay <mjlemay@gmail.com>
2024-08-18 15:29:06 -0700

    resizes editor screen dynamically on load

--- src/components/workspaceScreen.tsx
+++ src/components/workspaceScreen.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Modal from './modal';
 import ProjectForm from './projectForm';
 import Block from './block';
@@ -10,6 +10,7 @@ import { defaultProject, basicProjectType } from '../constants/defaults';
 import TabHeaders from './tabheaders';
 import { BackpackIcon } from '@radix-ui/react-icons';
 import FlowGraph from './flowGraph';
+import { useMeasure } from '@uidotdev/usehooks';
 
 
 
@@ -22,8 +23,11 @@ interface WorkspaceScreenProps {
 export default function WorkspaceScreen(props:WorkspaceScreenProps):JSX.Element {
   const { selectedProject = defaultProject, screenActionHandler = ()=>{}} = props;
   const [ selectedModal, setSelectedModal ] = useState('');
+  const [ wsSize, setWsSize ] = useState({width: 0, height: 0});
   const [ openModal, setOpenModal ] = useState(false);
   const { confirmDelete } = textCopy;
+  const [workspaceRef, { height:wsHeight, width:wsWidth }] = useMeasure();
+  console.log('wsHeight', wsHeight);
 
   const activateModal = (modal:string) => {
     setSelectedModal(modal);
@@ -60,12 +64,19 @@ export default function WorkspaceScreen(props:WorkspaceScreenProps):JSX.Element 
     }
   }
 
+  useEffect(()=> {
+    const { height } = wsSize;
+    if (height == 0 && wsHeight && wsWidth) {
+      setWsSize({...wsSize, height: wsHeight, width: wsWidth});
+    }
+  }, [wsHeight, wsWidth, setWsSize]);
+
   return (
-    <div className="flex flex-col min-h-full min-w-full min-h-full justify-top flex-wrap">
+    <div className="flex flex-col min-h-screen min-w-full max-h-screen max-w-full justify-top flex-nowrap">
       <div className="flex-full min-w-full justify-top max-h-[40px]">
         <TabHeaders projectTabs={[selectedProject]} actionHandler={handleActions} />
       </div>
-      <div data-name="workLayout" className="min-w-full justify-stretch items-stretch flex flex-row flex-grow min-h-full">
+      <div data-name="workLayout" className='min-w-full max-h-screen justify-stretch items-stretch flex flex-row flex-grow'>
           <div data-name="sidebar" className="min-w-[250px]">
             <Card>
               <Block title="Rulebooks" icon={<RuleIcon />} >
@@ -73,12 +84,15 @@ export default function WorkspaceScreen(props:WorkspaceScreenProps):JSX.Element 
               </Block>
             </Card>
           </div>
-          <div data-name="stage" className="grow min-h-full rounded-lg bg-neutral-950 mt-2 mb-2 mr-2">
-            <Block title="Core Rules" size="lg"  icon={<RuleIcon />}>
-              <div className="flex flex-grow min-w-full min-h-full justify-stretch items-stretch">
-                <FlowGraph />
+          <div data-name="stage" ref={workspaceRef as any} className="grow min-h-fit max-h-full rounded-lg bg-neutral-950 mt-2 mb-2 mr-2">
+            <Block title="Core Rules" size="lg" icon={<RuleIcon />} >
+              <div
+                className="block  m-0 p-0 overflow-auto"
+                style={{height: wsSize.height - 100 }}
+              >
+                <FlowGraph width={`${wsSize.width - 60}px`} height={`${wsSize.height - 100}px`} />
               </div>
-            </Block>
+              </Block>
           </div>
       </div>
 
